401f8df94bf32f1bdf142a4dc3dc877a755b1625
Srikanth2631 <srikanthsrihari50@gmail.com>
2025-04-08 19:24:20 +0530

    Add files via upload

---
+++ collection.js
@@ -0,0 +1,51 @@
+  /* var productContainer = document.getElementById("product")
+var search = document.getElementById("search")
+var productlist = productContainer.querySelectorAll("div")
+
+search.addEventListener("keyup",function(event){
+    var enteredValue = event.target.value.toUpperCase()
+    
+    for(count=0;count<productlist.length;count++)
+    {
+        var productname = productlist[count].querySelector("p").textContent
+        if(productname.toUpperCase().indexOf(enteredValue)<0)
+        {
+            productlist[count].style.display = "none"
+        }
+        else
+        {
+            productlist[count].style.display = "block"
+        }
+    }
+})*/
+var productContainer = document.getElementById("product");
+if (productContainer) {
+    var search = document.getElementById("search")
+    var productlist = productContainer.querySelectorAll("div")
+
+    search.addEventListener("keyup", function (event) {
+        var enteredValue = event.target.value.toUpperCase()
+        for (let count = 0; count < productlist.length; count++) {
+            var productname = productlist[count].querySelector("p").textContent
+            if (productname.toUpperCase().indexOf(enteredValue) < 0) {
+                productlist[count].style.display = "none"
+            } else {
+                productlist[count].style.display = "block"
+            }
+        }
+    });
+}
+
+
+
+
+var sidenav = document.querySelector(".side-navbar")
+
+function showNavbar()
+{
+   sidenav.style.left = "0"
+}
+function closeNavbar()
+{
+    sidenav.style.left = "-60%"
+}
